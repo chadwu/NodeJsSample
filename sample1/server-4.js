1a9f03a40e01fa967e80b20da62731ed5caeccb8
chadwu <chadwu0973@gmail.com>
2018-08-22 00:43:48 +0800

    chg 5

--- sample1/server-4.js
+++ sample1/server-4.js
@@ -1,38 +1,39 @@
-'use strict';
-var http=require('http');
+const http = require('http');
 
-var server=http.createServer(function(req,res){
-	var urlHome = function(res) {	//return text
-		res.writeHead(200, {'Content-Type':'text/html'});
-		res.write('<html><body><p>This is Home Page.</p></body></html>');
-		res.end();
-	};
-	var urlTest = function(res) {	//return url string
-		res.writeHead(200, {'Content-Type':'text/html'});
-		res.write('<html><body><p>This is ' + res.str2 +  ' Page.</p></body></html>');
-		res.end();
-	};
-	var urlData = function(res) {	//return JSON data
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.write(JSON.stringify({ message: "Hello World"}));
-		res.end();
-	};
+const server = http.createServer((req, res) => {
+  const resq = res;
+  const urlHome = function (resq) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>This is Home Page.</p></body></html>');
+    res.end();
+  };
+  const urlTest = function (resq) {	// return url string
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`<html><body><p>This is ${resq.str2} Page.</p></body></html>`);
+    res.end();
+  };
+  const urlData = function (resq) {	// return JSON data
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ message: 'Hello World' }));
+    res.end();
+  };
 
-	var actions = {
-		"/":urlHome,
-		"/admin":urlHome,
-		["/test" + req.url.substring(5)]:urlTest,
-		"/data":urlData,
-	};
+  const actions = {
+    '/': urlHome,
+    '/admin': urlHome,
+    ['/test' + req.url.substring(5)]: urlTest,
+    '/data': urlData,
+  };
 
-	if(typeof actions[req.url.toLowerCase()] !== 'function'){ // 防止執行未定義的目錄
-		//throw new Error("Action not found.");
-		res.end('Invalid Request!');
-	} else {
-		res.str2 = req.url;	// add str2 attribute to res for urlTest()
-		actions[req.url.toLowerCase()](res);
-	}
+  if (typeof actions[req.url.toLowerCase()] !== 'function') { // 防止執行未定義的目錄
+    // throw new Error("Action not found.");
+    res.end('Invalid Request!');
+  } else {
+    // add str2 attribute to res for urlTest()
+    resq.str2 = req.url;
+    actions[req.url.toLowerCase()](res);
+  }
 });
 
 server.listen(18892);
-console.log("Node.js web server at port 18892 is running.");
+console.log('Node.js web server at port 18892 is running.');
