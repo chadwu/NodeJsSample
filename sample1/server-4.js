386f290b1e4cceedb7204c623ba5083a48695afa
吳欽旺 Chad Wu <Chad.Wu@etatung.com>
2018-08-22 15:49:24 +0800

    chg 6

--- sample1/server-4.js
+++ sample1/server-4.js
@@ -1,20 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  const resq = res;
-  const urlHome = function (resq) {
+  // 這個 200 就是 http status 200，代表成功的意思
+  // Content-Type 是指回應的內容是文字網頁
+  const urlHome = function (resq = res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><p>This is Home Page.</p></body></html>');
     res.end();
   };
-  const urlTest = function (resq) {	// return url string
+  const urlTest = function (resq = res) {	// return url string
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(`<html><body><p>This is ${resq.str2} Page.</p></body></html>`);
+    res.write(
+      '<!DOCTYPE html>'
+      + '<html><body>'
+      + '<h1>Url: ' + resq.str2 + '</h1>'
+      + '<p>Header: ' + JSON.stringify(res.headers) + '</p>'
+      + '</body></html>',
+    );
     res.end();
   };
-  const urlData = function (resq) {	// return JSON data
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({ message: 'Hello World' }));
+
+  const urlData = function (resq = res) {	// return JSON data
+    res.writeHead(200, { 'Content-Type': 'application/json;charset=UTF-8' });
+    res.write(JSON.stringify({ message: 'Hello World 還有中文' }));
     res.end();
   };
 
@@ -27,10 +35,12 @@ const server = http.createServer((req, res) => {
 
   if (typeof actions[req.url.toLowerCase()] !== 'function') { // 防止執行未定義的目錄
     // throw new Error("Action not found.");
+    // res.writeHead(404);
     res.end('Invalid Request!');
   } else {
     // add str2 attribute to res for urlTest()
-    resq.str2 = req.url;
+    res.str2 = req.url;
+    res.headers = req.headers;
     actions[req.url.toLowerCase()](res);
   }
 });
